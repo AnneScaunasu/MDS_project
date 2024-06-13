ae634902df2d1488f5d8e370b81b27dc6c30c36b
Delia Bianca Suciu <96663234+perpetuumdolore@users.noreply.github.com>
2024-06-13 22:00:33 +0300

    Delete ProjectMDS/src/main/java/com/project/ProjectMDS directory

--- ProjectMDS/src/main/java/com/project/ProjectMDS/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.ProjectMDS.repository;
-
-import com.project.ProjectMDS.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
-
